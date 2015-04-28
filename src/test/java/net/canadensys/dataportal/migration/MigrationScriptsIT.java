80df8f913b13942a3e224cfe08cf6c17afcf68e3
Christian Gendreau <christiangendreau@gmail.com>
2015-04-28 14:49:29 -0400

    use « changeLogSync » on initial scripts

--- src/test/java/net/canadensys/dataportal/migration/MigrationScriptsIT.java
+++ src/test/java/net/canadensys/dataportal/migration/MigrationScriptsIT.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import liquibase.Contexts;
+import liquibase.LabelExpression;
 import liquibase.Liquibase;
 import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
@@ -28,6 +30,15 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
  */
 public class MigrationScriptsIT {
 
+	private static final String PUBLIC_MASTER_SCRIPT = "script/liquibase/publicChangeLogFile-master.xml";
+	private static final String BUFFER_MASTER_SCRIPT = "script/liquibase/bufferChangeLogFile-master.xml";
+
+	private static final String PUBLIC_INITIAL_SCRIPT = "script/liquibase/publicChangeLogFile-initial.xml";
+	private static final String BUFFER_INITIAL_SCRIPT = "script/liquibase/bufferChangeLogFile-initial.xml";
+
+	private static final Contexts NO_CONTEXT = new Contexts();
+	private static final LabelExpression NO_LABEL = new LabelExpression();
+
 	@Test
 	public void testScripts() {
 
@@ -35,12 +46,26 @@ public class MigrationScriptsIT {
 				.addScript("/script/occurrence/create_occurrence_tables.sql")
 				.addScript("/script/occurrence/create_occurrence_tables_buffer_schema.sql").build();
 		Connection conn;
+		Liquibase liquibase;
+
 		try {
 			conn = ds.getConnection();
 
 			Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(conn));
-			Liquibase liquibase = new Liquibase("script/liquibase/publicChangeLogFile-master.xml", new ClassLoaderResourceAccessor(), database);
+
+			// set initial state
+			liquibase = new Liquibase(PUBLIC_INITIAL_SCRIPT, new ClassLoaderResourceAccessor(), database);
+			liquibase.changeLogSync(NO_CONTEXT, NO_LABEL);
+			liquibase = new Liquibase(BUFFER_INITIAL_SCRIPT, new ClassLoaderResourceAccessor(), database);
+			liquibase.changeLogSync(NO_CONTEXT, NO_LABEL);
+
+			// Test public master script
+			liquibase = new Liquibase(PUBLIC_MASTER_SCRIPT, new ClassLoaderResourceAccessor(), database);
 			liquibase.update((String) null);
+
+			// Test buffer
+			liquibase = new Liquibase(BUFFER_MASTER_SCRIPT, new ClassLoaderResourceAccessor(), database);
+			liquibase.update("buffer");
 
 			conn.close();
 		}
